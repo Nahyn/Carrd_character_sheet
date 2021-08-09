15a8ed090350efd9829809fdbb7fec374456a91a
Nahyn <benj33760@hotmail.fr>
2021-08-09 09:56:12 +0200

    Hotfix event calling

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function getData(dataFileURL_) {
 		},
 		
 		"success": function(data_) {
-			callEvent(window.EventTypes.DATA_SUCCESS, data_)
+			callEvent(window.EventTypes.DATA_SUCCESS, window, JSON.parse(data_))
 		},
 		
 		"error": function() {
@@ -28,10 +28,74 @@ function getData(dataFileURL_) {
 	})
 }
 
-window.addEventListener(window.EventTypes.DATA_SUCCESS, function(event_){
-	console.log(event_.detail)
-});
+var characterSelectId = "character_sheet_selector"
+function createCharacterSelection(data_) {
+	var selectorElement = $("<select>");
+	
+	var defaultSelected = $("<option>")
+		.attr("selected", true)
+		.attr("disabled", true)
+		.html("Select a character")
+		.appendTo(selectorElement)
+	;
+	
+	var getSheetId = function getSheetId(groupId_, sheetIndex_) {
+		return groupId_ + "____" + sheetIndex_
+	}
+	
+	var getSheet = function getSheet(sheetId_) {
+		var components = sheetId_.split("____")
+		return data_[components[0]][components[1]]
+	}
+	
+	selectorElement.on("change", function(event_){
+		var newSheet = getSheet(selectorElement.val());
+		console.log(newSheet)
+	})
+	
+	Object.keys(data_).forEach(function(groupId_){
+		var tmpOptGroup = $("<optgroup>");
+		tmpOptGroup.attr("label", groupId_);
+		
+		var characterArray = data_[groupId_];
+		characterArray.forEach(function(characterLine_, sheetIndex_) {
+			var characterName = "Character " + sheetIndex_
+			
+			if(characterLine_.info) {
+				var nameInfoLine = characterLine_.info.find(function(infoLine_){
+					return (["NOM", "NAME"]).indexOf(infoLine_.label.toUpperCase() != -1)
+				})
+				
+				if(nameInfoLine != undefined) {
+					characterName = nameInfoLine.value
+				}
+			}
+			
+			var sheetId = getSheetId(groupId_, sheetIndex_)
+			
+			var optionElement = $("<option>")
+				.attr("value", sheetId)
+				.html(characterName)
+				.appendTo(tmpOptGroup)
+		})
+		
+		tmpOptGroup.appendTo(selectorElement)
+	})
+	
+	return selectorElement;
+}
 
 window.addEventListener("load", function(){
+	window.addEventListener(window.EventTypes.DATA_SUCCESS, function(event_){
+		var data = event_.detail;
+		
+		var mainContainer = $($("#main > .inner")[0]);
+		var characterSelectorElement = createCharacterSelection(data)
+		$("#" + characterSelectId, mainContainer).remove()
+		mainContainer.append(characterSelectorElement)
+	});
+	
 	getData(dataUrl);
 })
+
+getData(dataUrl);
